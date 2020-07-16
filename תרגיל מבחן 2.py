0414a68569750d1429ea42141558f88a4871f1e4
maor90b <68278384+maor90b@users.noreply.github.com>
2020-07-16 09:54:41 +0300

    hey dude

---
+++ תרגיל מבחן 2.py
@@ -0,0 +1,14 @@
+list1=[]
+for i in range(1,11):
+    list1.append(i)
+print(list1)
+# print(list1[7:11])
+# print(list1[::-1])
+# print(list1[1::2])
+# print(list1[2::2])
+# print(list1[0:5])
+# print(list1[0::2])
+# list1[7:10]=[int(input("enter number:"))]
+# print(list1)
+print(list1[-3:-1])
+list2=list1[-3:-1]
